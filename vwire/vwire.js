132076efd2e1be6a8c5b569a09a7498a6e3396bf
arao <araobp@users.noreply.github.com>
2017-09-22 04:15:25 +0900

    abstraction

--- vwire/vwire.js
+++ vwire/vwire.js
@@ -71,7 +71,7 @@ module.exports = function(RED) {
                         if (parserEnabled) {
                             var startsWith = dataString.substring(0,1);
                             switch(startsWith) {
-                                case '$':
+                                case '$', '*':
                                     var resp = dataString.split(':');
                                     var command = resp[1];
                                     if (command in transactions) {
@@ -88,17 +88,17 @@ module.exports = function(RED) {
                                     if (resp[1] == 'NO_DATA') {
                                         converted = null;
                                     } else {
-                                        var data = resp[2].split(',');
+                                        var converted = resp[2].split(',');
                                         switch(resp[1]) {
                                             case 'FLOAT':
-                                                for (var d of data) {
-                                                    converted.push(parseFloat(d));
-                                                }
+                                                converted.map(function(elm) {
+                                                    return parseFloat(elm);
+                                                });
                                                 break;
                                             default:
-                                                for (var d of data) {
-                                                    converted.push(parseInt(d));
-                                                }
+                                                converted.map(function(elm) {
+                                                    return parseInt(elm);
+                                                });
                                                 break;
                                         }
                                     }
@@ -172,27 +172,29 @@ module.exports = function(RED) {
         });
     }
     RED.nodes.registerType("vwire-config", VwireConfig);
-
-    function Vwire(config) {
+    
+    function vwire(config) {
         RED.nodes.createNode(this, config);
         var node = this;
         var params = RED.nodes.getNode(config.params);
         var command = null;
-        if (config.name != '') {
+        if ('name' in config && config.name != '') {
             command = config.name;
         }
         var noack = config.noack;
         var port = getPort(params); 
         node.on('input', function(msg) {
             var cmd = null;
-            if (command) {
+            if (command != null) {
                 cmd = command;
             } else {
                 cmd = msg.payload;
             }
+            console.log(parserEnabled);
             if (parserEnabled) {
                 var cmd_name = cmd.split(':')[0]
                 transactions[cmd_name] = node;
+                console.log(transactions);
             } else {
                 transactions._next = node;
             }
@@ -206,7 +208,46 @@ module.exports = function(RED) {
             done();
         });
     }
-    RED.nodes.registerType("vwire", Vwire);
+    
+    function vwireMaker(cmd, noack) {
+        function vwire(config) {
+            RED.nodes.createNode(this, config);
+            var node = this;
+            var params = RED.nodes.getNode(config.params);
+            var port = getPort(params); 
+            node.on('input', function(msg) {
+                console.log(parserEnabled);
+                if (parserEnabled) {
+                    var cmd_name = cmd.split(':')[0]
+                    transactions[cmd_name] = node;
+                    console.log(transactions);
+                } else {
+                    transactions._next = node;
+                }
+                port.write(cmd + '\n');
+                if (noack) {
+                    node.send({payload: null});
+                }
+            });
+            node.on('close', function(removed, done) {
+                transactions = {};
+                done();
+            });
+        }
+        return vwire;
+    }
+    
+    RED.nodes.registerType("vwire", vwire);
+    
+    // The following nodes require ParserEnabled = true
+    RED.nodes.registerType("hall-sensor", vwireMaker("SEN:17", false));
+    RED.nodes.registerType("accelerometer", vwireMaker("SEN:19", false));
+    RED.nodes.registerType("temperature-humidity", vwireMaker("SEN:20", false));
+
+    // The following nodes require ParserEnabled = false
+    RED.nodes.registerType("door-status", vwireMaker("07", false));
+    RED.nodes.registerType("servo-unlock", vwireMaker("150", false));
+    RED.nodes.registerType("servo-lock", vwireMaker("1590", false));
 
     function VwireIn(config) {
         RED.nodes.createNode(this, config);
